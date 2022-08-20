ba5332bc4d0bef07f0c47a828479a32027d7cc8a
furgelisherpa <furgeli3@gmail.com>
2022-08-20 06:18:55 +0000

    first commit

---
+++ config.def.h
@@ -0,0 +1,21 @@
+/* See LICENSE file for copyright and license details. */
+
+/* interval between updates (in ms) */
+const unsigned int interval = 1000;
+
+/* text to show if no value can be retrieved */
+static const char unknown_str[] = "n/a";
+
+/* maximum output string length */
+#define MAXLEN 2048
+
+static const struct arg args[] = {
+	/* function format          argument */
+	{ run_command, "[  %2s%% ] ", "pamixer --get-volume" },
+	{ cpu_perc, "[  %s%% ] ", NULL	      },
+	{ ram_perc, "[  %s%% ] ", NULL	      },
+	{ disk_perc , "[  %s%% ] ", "/home"	      },
+	{ run_command, "[ %s ] ", "date '+ %b %d (%a)' " },
+	{ run_command, "[ %s ]", "date '+ %I:%M%P'" },
+};
+
