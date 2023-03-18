fdcda7e39333a12624f157b9268f1c234b8515ee
furgelisherpa <CoffeeWise@proton.me>
2023-03-18 22:52:55 +0545

    module added

--- config.def.h
+++ config.def.h
@@ -4,15 +4,17 @@
 const unsigned int interval = 1000;
 
 /* text to show if no value can be retrieved */
-static const char unknown_str[] = "n/a";
+static const char unknown_str[] = "";
 
 /* maximum output string length */
 #define MAXLEN 2048
 
 static const struct arg args[] = {
 	/* function format          argument */
-	{ run_command, "🔊 %2s%% | ", "pamixer --get-volume" },
-	{ run_command, "%s | ", "date '+📅 %b %d (%a)' " },
-	{ run_command, "%s ", "date '+🕖 %I:%M%P'" },
+	{ run_command, "%s | ", "sys-updates" },
+	{ run_command, "%s | ", "internet" },
+	{ run_command, "󰕾 %2s%% | ", "pamixer --get-volume" },
+	{ run_command, "%s | ", "date '+󰃭 %b %d (%a)' " },
+	{ run_command, "%s", "date '+󱑑 %I:%M%P'" },
 };
 
